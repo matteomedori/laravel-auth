d0455594ac0ded61d78efa052da61455a6ca8336
matteomedori <matteo.medori.97@gmail.com>
2024-02-06 17:48:38 +0100

    feat: aggiunta table per mostrare progetti

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,10 +1,26 @@
 @extends('layouts.admin')
 
 @section('content')
+    <h2 class="my-3">Lista dei progetti</h2>
     <a href="{{ route('admin.projects.create') }}">Aggiungi un nuovo progetto</a>
-    <ul>
-        @foreach ($projects as $project)
-            <li>{{ $project->title }}</li>
-        @endforeach
-    </ul>
+    <table class="table table-striped mt-2">
+        <thead>
+            <tr>
+                <th scope="col">Titolo</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Linguaggi</th>
+                <th scope="col">Frameworks</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->description }}</td>
+                    <td>{{ $project->languages }}</td>
+                    <td>{{ $project->frameworks }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
